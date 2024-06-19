16398c1faaa848ad60d14b1b920263b1acb1e2d6
jconan <conanjahh@hotmail.com>
2024-06-19 19:41:36 +0200

    fix navbar hamburger mode
    it is now clickable and can choose menu items

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,12 +1,20 @@
+<script lang="ts">
+	let isActiveClass = '';
+	function toggle() {
+		isActiveClass = isActiveClass === '' ? 'is-active' : '';
+	}
+</script>
+
 <nav class="navbar" aria-label="main navigation">
 	<div class="navbar-brand">
 		<slot name="brand" />
 
 		<button
-			class="navbar-burger"
+			class="navbar-burger {isActiveClass}"
 			aria-label="menu"
 			aria-expanded="false"
 			data-target="navbar-items"
+			on:click={toggle}
 		>
 			<span aria-hidden="true"></span>
 			<span aria-hidden="true"></span>
@@ -15,7 +23,9 @@
 		</button>
 	</div>
 
-	<div id="navbar-items" class="navbar-menu">
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div id="navbar-items" class="navbar-menu {isActiveClass}" on:click={toggle}>
 		<div class="navbar-start">
 			<slot />
 		</div>

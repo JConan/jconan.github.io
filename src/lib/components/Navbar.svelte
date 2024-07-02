ee62d760b3e120c7771df55ffb3bb013b75823da
jconan <conanjahh@hotmail.com>
2024-07-02 09:49:19 +0200

    add basic dark-light theme button

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,26 @@
 <script lang="ts">
 	import { Sun, Moon } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 	let isActiveClass = '';
 	function toggle() {
 		isActiveClass = isActiveClass === '' ? 'is-active' : '';
+	}
+
+	let isDarkTheme: boolean | undefined = undefined;
+
+	onMount(() => {
+		const bodyTheme = document.documentElement.getAttribute('data-theme');
+		isDarkTheme = bodyTheme
+			? bodyTheme === 'dark'
+			: window.matchMedia('(prefers-color-scheme: dark)').matches;
+	});
+
+	function toggleTheme() {
+		isDarkTheme = !isDarkTheme;
+	}
+
+	$: if (isDarkTheme !== undefined) {
+		document.documentElement.setAttribute('data-theme', isDarkTheme ? 'dark' : 'light');
 	}
 </script>
 
@@ -33,7 +51,13 @@
 			</div>
 
 			<div class="navbar-end">
-				<button class="button is-ghost is-small navbar-item"><Moon /></button>
+				<button on:click={toggleTheme} class="button is-ghost is-small navbar-item">
+					{#if isDarkTheme}
+						<Sun />
+					{:else}
+						<Moon />
+					{/if}
+				</button>
 			</div>
 		</div>
 	</nav>

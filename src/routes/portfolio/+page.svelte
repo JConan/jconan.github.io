ab3f99d692fc74f97ae2bb079d231fef6e0a0677
jconan <conanjahh@hotmail.com>
2024-07-06 03:48:45 +0200

    add first project

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,1 +1,41 @@
-<p>Check my project</p>
+<script lang="ts">
+	let iframe: HTMLIFrameElement;
+	let height = 400;
+	let isLoaded = false;
+
+	function getHeightHandler(event: MessageEvent) {
+		if (event.data.type === 'getHeight') {
+			console.log({ data: event.data });
+			height = event.data.height + 10;
+			isLoaded = true;
+		}
+	}
+
+	function getHeight() {
+		window.addEventListener('message', getHeightHandler);
+		iframe.contentWindow?.postMessage('getHeight', '*');
+	}
+</script>
+
+<p>Tic Tac Toe</p>
+
+<div style="opacity: {isLoaded ? 1 : 0}">
+	<iframe
+		bind:this={iframe}
+		title="tic-tac-toe"
+		src="https://jconan.github.io/fm-tic-tac-toe/"
+		frameborder="0"
+		class="container"
+		{height}
+		on:load={getHeight}
+	></iframe>
+</div>
+
+<style>
+	div {
+		transition: all 1s;
+		& iframe {
+			border-radius: 15px;
+		}
+	}
+</style>

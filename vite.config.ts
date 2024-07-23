b331077b589f883ec9277770599cba038a80f3e1
JConan <conanjahh@hotmail.com>
2024-07-23 16:16:20 +0200

    add vite imagetools for image optimisation

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { imagetools } from 'vite-imagetools';
 
-import type { KIT_ROUTES } from '$lib/ROUTES'
-import { kitRoutes } from 'vite-plugin-kit-routes'
+import type { KIT_ROUTES } from '$lib/ROUTES';
+import { kitRoutes } from 'vite-plugin-kit-routes';
 
 export default defineConfig({
-	plugins: [sveltekit(), kitRoutes<KIT_ROUTES>({
-
-		
-	})],
+	plugins: [sveltekit(), kitRoutes<KIT_ROUTES>({}), imagetools()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}

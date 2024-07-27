81bcd4ec4e08ff4ce0da60267c9268d844409a84
JConan <JConan@users.noreply.github.com>
2024-07-27 12:46:56 +0000

    deploy: 74f862bcd20d84240e0f29ab0fedd66870b916e4

--- _app/immutable/chunks/ROUTES.DS7TGnZc.js
+++
@@ -1,1 +0,0 @@
-const r={"/":"/","/contact":"/contact","/cv":"/cv","/portfolio":"/portfolio","/portfolio/demo/[slug]":o=>`/portfolio/demo/${o.slug}`},s={},e={},l={},t={...r,...e,...s,...l};function i(o,...n){if(t[o]instanceof Function){const c=t[o];return c(...n)}else return t[o]}export{i as r};
---
+++ _app/immutable/nodes/5.BuslBVh2.js
@@ -0,0 +1,1 @@
+import{P as e,a as n}from"../chunks/5.C-2rUGmv.js";export{e as component,n as universal};
